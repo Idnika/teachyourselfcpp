eae21fcbf35eca3010d897820b1f92070ad1ab0d
Idnika <sizna35@gmail.com>
2019-09-12 05:53:09 +0900

    Add files via upload
    42p_get string length

---
+++ 20190912_strlen.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+  string a;
+  cout << "Input string: ";
+  getline(cin, a);
+  cout << a.size() << "\n";
+
+  return 0;
+}
