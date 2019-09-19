11e96abd2bde4efc6a60f2b36ffd8e325a436991
Idnika <sizna35@gmail.com>
2019-09-19 10:53:58 +0900

    new using pointer

---
+++ 20190919_new_pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class samp {
+  int i, j;
+public:
+  void set_ij(int a, int b) { i = a; j = b;}
+  int get_product() { return i * j; }
+};
+
+int main()
+{
+  samp *p;
+
+  p = new samp;
+  if(!p) {
+    cout << "Allocation error!" << endl;
+    return 1;
+  }
+
+  p->set_ij(4,5);
+
+  cout << "Product is: " << p->get_product() << endl;
+
+  return 0;
+}
