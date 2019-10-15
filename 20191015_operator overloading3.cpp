1b690b4e28b3a5d812e80eb7c40e0edd0970bbcf
Idnika <sizna35@gmail.com>
2019-10-15 17:52:17 +0900

    operator overloading example3

---
+++ 20191015_operator overloading3.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class coord {
+  int x, y;
+public:
+  coord() { x = 0; y = 0; }
+  coord(int i, int j) { x = i; y = j; }
+  void get_xy(int &i, int &j) { i = x; j = y; }
+  coord operator+(coord ob2);
+  coord operator-(coord ob2);
+  coord operator*(coord ob2);
+  coord operator/(coord ob2);
+};
+
+coord coord::operator+(coord ob2) {
+  coord temp;
+
+  temp.x = x + ob2.x;
+  temp.y = y + ob2.y;
+
+  return temp;
+}
+
+coord coord::operator-(coord ob2) {
+  coord temp;
+
+  temp.x = x - ob2.x;
+  temp.y = y - ob2.y;
+
+  return temp;
+}
+
+coord coord::operator/(coord ob2) {
+  coord temp;
+
+  temp.x = x / ob2.x;
+  temp.y = y / ob2.y;
+
+  return temp;
+}
+
+coord coord::operator*(coord ob2) {
+  coord temp;
+
+  temp.x = x * ob2.x;
+  temp.y = y * ob2.y;
+
+  return temp;
+}
+
+int main()
+{
+  coord o1(10, 10), o2(5, 2), o3;
+  int x, y;
+
+  o3 = o1 + o2;
+  o3.get_xy(x, y);
+  cout << "o1 + o2 = " << x << ", " << y << endl;
+
+  o3 = o1 - o2;
+  o3.get_xy(x, y);
+  cout << "o1 - o2 = " << x << ", " << y << endl;
+
+  o3 = o1 * o2;
+  o3.get_xy(x, y);
+  cout << "o1 * o2 = " << x << ", " << y << endl;
+
+  o3 = o1 / o2;
+  o3.get_xy(x, y);
+  cout << "o1 / o2 = " << x << ", " << y << endl;
+
+  return 0;
+}
