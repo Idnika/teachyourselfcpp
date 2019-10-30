c11c0a5d0e3347b4705212718874f867920ddc1f
Idnika <sizna35@gmail.com>
2019-10-30 18:41:20 +0900

    Add files via upload

---
+++ 20191030_initializing derived class.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class base {
+  int i;
+public:
+  base(int n) {
+    cout << "Construcing base class" << endl;
+    i = n;
+  }
+  ~base() {
+    cout << "Destructing base class" << endl;
+  }
+  void showi() { cout << i << endl; }
+};
+
+class derived : public base {
+  int j;
+public:
+  derived(int n, int m) : base(m) {
+    cout << "Constructing derived class" << endl;
+    j = n;
+  }
+  ~derived() { cout << "Desturcting derived class" << endl; }
+  void showj() { cout << j << endl; }
+};
+
+int main()
+{
+  derived o(10, 20);
+
+  o.showi();
+  o.showj();
+
+  return 0;
+}
