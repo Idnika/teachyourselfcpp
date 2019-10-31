755cbd52cf5d094c4d7889f42741736c86f6d809
Idnika <sizna35@gmail.com>
2019-10-31 18:06:41 +0900

    derived class practice1

---
+++ 20191031_derived practice1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class mybase {
+  char str[80];
+public:
+  mybase(char *s) { strcpy(str, s); }
+  char *get() { return str; }
+};
+
+class myderived : public mybase {
+  int len;
+public:
+  myderived(char *s) : mybase(s) {
+    len = strlen(s);
+  }
+  int getlen() { return len; }
+  void show() { cout << get() << endl; }
+};
+
+int main()
+{
+  myderived ob("hello");
+
+  ob.show();
+  cout << ob.getlen() << endl;
+
+  return 0;
+}
